8db0d84d6635a80ad9137505efeb6c13b1406d5a
Anthony Le <leanthony104@yahoo.com>
2024-08-04 16:48:53 -0500

    using variables

--- CTA1_Option1.java
+++ CTA1_Option1.java
@@ -1,9 +1,17 @@
+
 public class CTA1_Option1 {
+
     public static void main(String[] args) {
-        System.out.println("Tony");
-        System.out.println("Stark");
-        System.out.println("10880 Malibu Point");
-        System.out.println("Malibu");
-        System.out.println("90265");
+        String firstName = "Tony";
+        String lastName = "Stark";
+        String Address = "10880 Malibu Point";
+        String City = "Malibu";
+        String zipCode = "90265";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(Address);
+        System.out.println(City);
+        System.out.println(zipCode);
     }
 }
